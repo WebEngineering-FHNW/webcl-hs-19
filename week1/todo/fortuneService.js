bc747232390ba6dd3191ea69f33de6e32d2078c4
Dierk König <dierkkonig@felicitass-macbook-pro.home>
2019-09-16 21:39:10 +0200

    before week 1

---
+++ week1/todo/fortuneService.js
@@ -0,0 +1,16 @@
+// stand-in for an asynchronous service
+
+const fortunes = [
+    "Do the WebPr Homework",
+    "Care for the JavaScript Toolbox",
+    "Watch the recommended videos",
+    "Read the recommended chapters in You-dont-know-JS",
+    "Do the dataflow excel challenge!"
+];
+
+function fortuneService(whenDone) {
+    setTimeout(
+        () => whenDone(fortunes[Math.floor((Math.random() * fortunes.length))]),
+        Math.floor((Math.random() * 3000))
+    );
+}
